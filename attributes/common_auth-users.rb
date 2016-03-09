8995a7550f6b9eeff0aa1fa9769ddbfd5cd8d4fb
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-08 23:15:05 -0500

    Rename attribute files

--- attributes/common_auth-users.rb
+++ attributes/common_auth-users.rb
@@ -1,11 +1,7 @@
 
-#
-# Users
-#
-
 # Data bag to fetch users definitions from
 #
-default[:common_auth][:users][:data_bag] = "users"
+default[:common_auth][:users][:data_bag] = "auth_users"
 
 # Hash containing a list of managed users overrides
 #
@@ -20,21 +16,4 @@ default[:common_auth][:users][:managed] = Array.new
 #
 default[:common_auth][:users][:defaults][:home] = "/home"
 default[:common_auth][:users][:defaults][:shell] = "/bin/false"
-
-#
-# Groups
-#
-
-# Data bag to fetch groups definitions from
-#
-default[:common_auth][:groups][:data_bag] = "groups"
-
-# Hash containing a list of managed groups overrides
-#
-default[:common_auth][:groups][:config] = Mash.new
-
-# Array containing a list of managed groups
-# ** This should never be manually modified
-#
-default[:common_auth][:groups][:managed] = Array.new
 
