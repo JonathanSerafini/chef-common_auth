00b09f2cc7f63378d54b9cdd7f67ce1c17243e9f
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-08 23:23:18 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures chef_common_auth'
 long_description 'Installs/Configures chef_common_auth'
 version '0.1.2'
 
-depends 'common_linux'
+depends 'common_linux' # Gem installation
+depends 'common_attrs' # Library helpers
 depends 'sudo'
 depends 'openssh'
