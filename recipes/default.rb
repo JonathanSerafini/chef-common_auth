19ec9f0b7027e3f314c2077caae8201fc5d47041
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-12 13:20:43 -0500

    Ensure common_linux is included for attribute loading

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,7 @@
 
+# Ensure common_linux runs firs so as to install required packages
+include_recipe "common_linux::default"
+
 include_recipe "#{cookbook_name}::auth"
 include_recipe "#{cookbook_name}::sudo"
 include_recipe "#{cookbook_name}::openssh"
