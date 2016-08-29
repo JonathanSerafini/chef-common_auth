2cb0e973541cc1b84a302def9e46a5e3d5d9e568
Jonathan Serafini <jonathan@serafini.ca>
2016-08-29 14:51:45 -0400

    Update minimal policyfile

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,33 +1,11 @@
-
-name "common_auth"
+name 'common_auth'
 default_source :supermarket
 
-default[:common_auth][:groups][:config][:devops] = true
-
-default[:common_auth].tap do |config|
-  config[:sudoers][:ubuntu].tap do |sudoer|
-    sudoer[:user] = "ubuntu"
-    sudoer[:runas] = "root"
-    sudoer[:nopasswd] = true
-  end
-
-  config[:sudoers][:deploy].tap do |sudoer|
-    sudoer[:group] = "deploy"
-    sudoer[:runas] = "root"
-    sudoer[:nopasswd] = true
-    sudoer[:commands] = ["/usr/bin/chef-client"]
-  end
-
-  config[:sudoers]["90-cloud-init-users"].tap do |sudoer|
-    sudoer[:action] = "remove"
-  end
+default['common_auth']['groups']['config'].tap do |config|
+  #config['devops']['members'] = ['']
+  config['devops']['action'] = 'create'
 end
 
-default[:authorization][:sudo][:groups] = %w(devops)
-default[:openssh][:server][:allow_groups] = %w(devops deploy)
-
-run_list "common_auth::default"
-cookbook "common_auth", path: "."
-cookbook "common_linux", path: "../common_linux"
-cookbook "common_attrs", path: "../common_attrs"
-cookbook "rsyslog_ng", path: "../rsyslog_ng"
+run_list 'common_auth::default'
+cookbook 'common_auth', path: '.'
+cookbook 'common_attrs'
