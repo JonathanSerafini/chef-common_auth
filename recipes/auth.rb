7a6631ffd2873afef0803f35cd67053d2f816bb3
Jonathan Serafini <jonathan@serafini.ca>
2016-08-31 08:56:58 -0400

    Move user/group loading to custom DSL from recipe

--- recipes/auth.rb
+++ recipes/auth.rb
@@ -1,110 +1,16 @@
 # Install the secure_data_bag gem
 require 'secure_data_bag'
 
-# Enqueue user and group items to manage
-# - managed: items that were managed during a previous run
-# - config: items that _may_ be managed during this run
-#
-users_requested = [
-  node[:common_auth][:users][:config].keys
-].flatten.compact.uniq
+# Include common_auth business logic methods
+send(:extend, CommonAuth::DSL)
 
-users_managed = [
-  node[:common_auth][:users][:managed].keys,
-  node[:common_auth][:users][:config].keys
-].flatten.compact.uniq
-
-groups_requested = [
-  node[:common_auth][:groups][:config].keys
-].flatten.compact.uniq
-
-groups_managed = [
-  node[:common_auth][:groups][:managed].keys,
-  node[:common_auth][:groups][:config].keys
-].flatten.compact.uniq
-
-# Fetch group data_bag items
-#
-groups = search(node[:common_auth][:groups][:data_bag], "id:*").map do |item|
-  # Apply databag namespace if present
-  #
-  item_name = item["name"] || item["id"]
-  item = item.to_common_namespace
-  item["name"] ||= item_name
-
-  # Apply attribute override if present
-  #
-  override = node[:common_auth][:groups][:config].fetch(item_name, {})
-  item = Chef::Mixin::DeepMerge.merge(item, override)
-
-  # Ensure that this item is meant to be managed
-  #
-  next if not groups_managed.include?(item_name)
-
-  # Ensure that strays are deleted
-  #
-  if not groups_requested.include?(item_name)
-    item["action"] = "remove"
-  end
-
-  # Set default action to :nothing if no action provided
-  # - Perhaps this is only used to generate a list of users
-  item["action"] ||= "nothing"
-
-  # Enqueue group members 
-  #
-  if item["include_members"]
-    members = Array(item["members"])
-    users_requested.concat(members)
-    users_managed.concat(members)
-  end
-
-  # Remove unsupported keys
-  #
-  item.delete("include_members")
-
-  # Return item
-  #
-  item
-end.compact
-
-# Fetch user data_bag items
-#
-users = search(node[:common_auth][:users][:data_bag], "id:*").map do |item|
-  # Apply databag namespace if present
-  #
-  item = SecureDataBag::Item.from_item(item)
-  item = item.to_common_namespace
-  item_name = item["name"] || item["id"]
-
-  # Apply attribute override if present
-  #
-  override = node[:common_auth][:users][:config].fetch(item_name, {})
-  item = Chef::Mixin::DeepMerge.merge(item, override)
-
-  # Ensure that this item is meant to be managed
-  #
-  next if not users_managed.include?(item_name)
-
-  # Ensure that strays are deleted
-  #
-  if not users_requested.include?(item_name)
-    item["action"] = "remove"
-  end
-
-  # Set default action to :nothing if no action provided
-  #
-  item["action"] ||= "nothing"
-
-  # Return item
-  #
-  item
-end.compact
+load_common_auth_groups
+load_common_auth_users
 
 # Create user resources
 #
-users.each do |item|
-  common_user_account item["name"] || item["id"] do
+common_auth_users_state.each do |name, item|
+  common_user_account name do
     common_properties(item)
   end
 end
@@ -118,8 +24,12 @@ end
 
 # Create group resources
 #
-groups.each do |item|
-  common_group_account item["name"] || item["id"] do
+common_auth_groups_state.each do |name, item|
+  common_group_account name do
     common_properties(item)
   end
 end
+
+# Cleanup run_state
+#
+common_auth_clear_cache
